bee326b1e02ebac07e1fa932f15be3c118dc987f
rui <szsbxjhc@163.com>
2019-11-11 10:59:22 +0800

    上传项目_springboot入门

---
+++ day01_springboot_demo01/src/main/java/com/itheima/application.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class application {
+    public static void main(String[] args) {
+        SpringApplication.run(application.class, args);
+    }
+}
