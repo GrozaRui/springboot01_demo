2ecd0599d0d9697da0b44e388c8c698c4296f3bc
rui <szsbxjhc@163.com>
2019-11-11 11:13:20 +0800

    devtest分支上传

--- day01_springboot_demo01/src/main/java/com/itheima/controller/HelloController.java
+++ day01_springboot_demo01/src/main/java/com/itheima/controller/HelloController.java
@@ -3,11 +3,17 @@ package com.itheima.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("this is a git update branches");
         return "hello!SpringBoot!";
     }
 }
